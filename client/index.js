e3c5c900d73bb2ccced7c326c9f54814e12004f7
Fernando Brandao <fwbrandao@gmail.com>
2019-06-18 08:29:58 +0100

    signup form

--- client/index.js
+++ client/index.js
@@ -7,6 +7,7 @@ import { ApolloProvider } from 'react-apollo';
 
 import App from './components/App';
 import LoginForm from './components/LoginForm';
+import SignupForm from './components/SignupForm';
 
 const networkInterface = new createNetworkInterface({
     uri: '/graphql',
@@ -28,6 +29,7 @@ const Root = () => {
             <Router history={hashHistory}>
                 <Route path="/" component={App}>
                     <Route path="login" component={LoginForm} />
+                    <Route path="signup" component={SignupForm} />
                 </Route>
             </Router>
         </ApolloProvider>
